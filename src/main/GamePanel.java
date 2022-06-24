c6390a3bcbc3d00ac0df77d25d25271859a79347
Jorge Luis Villarreal Treviño <jorge.lvt@outlook.com>
2022-06-24 19:16:32 +0200

    Added FPS to refresh the screen every certain number of frames.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,6 +15,9 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    // FPS
+    int FPS = 60;
+
     KeyHandler keyH = new KeyHandler();
     Thread gameThread; // Enables FPS
 
@@ -42,19 +45,39 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
-      while(gameThread != null) {
+        // 1.- UPDATE: information such as character positions
+        // 2.- DRAW: draw the screen with updated information
 
-          long currentTime = System.nanoTime();
-          System.out.println("current Time:" + currentTime);
+        double drawInterval = 1000000000/FPS; // 0.0166666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
 
-          // 1 UPDATE: information such as character positions
-          update();
+        while (gameThread != null) {
 
-          // 2 DRAW: draw the screen with updated information
-          repaint();
-      }
+            currentTime = System.nanoTime();
 
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
     }
     public void update() {
 
